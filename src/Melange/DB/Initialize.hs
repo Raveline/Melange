6c6dde0410d2f5d7dfc3828a2e05ffc8929e1a35
Raveline <eraveline@gmail.com>
2018-07-29 19:08:11 +0200

    Add the initial migration logic

---
+++ src/Melange/DB/Initialize.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Melange.DB.Initialize
+  (
+    initDB
+  ) where
+
+import           Control.Monad               (void)
+import           Control.Monad.Writer
+import           Data.ByteString.Char8       (ByteString, pack)
+import           Data.List                   (intercalate)
+import           Data.Maybe                  (fromMaybe)
+import           Melange.DB.Schema
+import           Squeal.PostgreSQL
+import           Squeal.PostgreSQL.Migration
+import           System.Environment          (lookupEnv)
+
+fetchEnv :: String -> IO String
+fetchEnv env =
+  let onError = error $ "Missing environment variable " <> env
+  in fromMaybe onError <$> lookupEnv env
+
+fetcher :: String -> WriterT [String] IO ()
+fetcher env = do
+  value <- liftIO $ fetchEnv env
+  tell [env <> "=" <> value]
+
+computeConnectionString :: IO ByteString
+computeConnectionString =
+  let computation =
+        fetcher "host"
+        >> fetcher "port"
+        >> fetcher "dbname"
+        >> fetcher "user"
+        >> fetcher "password"
+      formatter = pack . unwords
+  in formatter <$> execWriterT computation
+
+initDB :: IO ()
+initDB = do
+  connectionString <- computeConnectionString
+  void $ withConnection connectionString $
+    manipulate (UnsafeManipulation "SET client_min_messages TO WARNING;")
+    & pqThen (migrateUp $ single setup)
+  putStrLn "Finished"
