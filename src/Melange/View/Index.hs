70136055f40da6f977f937c70be943485a80bfff
Raveline <eraveline@gmail.com>
2018-08-04 14:22:43 +0200

    Add draft for raw index

--- src/Melange/View/Index.hs
+++ src/Melange/View/Index.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Melange.View.Index
   (
+    IndexPage (..)
   ) where
 
-import Control.Monad (forM_)
-import Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes as A
+import           Control.Monad               (forM_)
+import           Melange.Model.External
+import           Text.Blaze.Html5            as H
+import           Text.Blaze.Html5.Attributes as A
 
-index = docTypeHtml $ do
-  H.head $
-    H.title "Stuff"
-  body $
-    p "Stuff"
+newtype IndexPage = IndexPage (Maybe Board)
+
+instance ToMarkup IndexPage where
+  toMarkup (IndexPage Nothing) = do
+    H.head $
+      H.title "Melange"
+    body $
+      p "There is nothing to display"
+  toMarkup (IndexPage (Just b))= do
+    H.head $
+      H.title "Melange"
+    body $
+      p "There should be a pretty board here"
