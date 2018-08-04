9293f9ca27442273de4321ed17a2dd295b07c9fc
Raveline <eraveline@gmail.com>
2018-08-04 14:51:50 +0200

    Handle CORS and add logging middleware

--- src/Melange/Service.hs
+++ src/Melange/Service.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Melange.Service
   (
     runServer
   ) where
 
 import           Data.Proxy
-import qualified Melange.API.API            as API (API, server, MelangePool)
-import           Melange.DB.Initialize      (computeConnectionString)
+import qualified Melange.API.API             as API (API, MelangePool, server)
+import           Melange.DB.Initialize       (computeConnectionString)
 import           Network.Wai
 import           Network.Wai.Handler.Warp
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Network.Wai.Middleware.Cors (CorsResourcePolicy (..), cors,
+                                              simpleCorsResourcePolicy)
 import           Servant
 import           Squeal.PostgreSQL.Pool
 
@@ -17,9 +21,16 @@ melangeAPI = Proxy
 melange :: API.MelangePool -> Application
 melange pool = serve melangeAPI (API.server pool)
 
+localhostCors :: CorsResourcePolicy
+localhostCors = simpleCorsResourcePolicy { corsOrigins = Just (["http://localhost:8080"], True)
+                                         , corsRequestHeaders = ["Content-Type"] }
+
+corsMiddleware :: Middleware
+corsMiddleware = cors (const $ Just localhostCors)
+
 runServer :: IO ()
 runServer =
-  getPool >>= run 8492 . melange
+  getPool >>= run 8492 . corsMiddleware . logStdoutDev . melange
 
 getPool :: IO API.MelangePool
 getPool = do
