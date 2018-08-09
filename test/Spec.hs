71c8727b614f526cafd8f4152790e60816830bc1
Raveline <eraveline@gmail.com>
2018-08-09 21:44:44 +0200

    Add specs for deletion and update queries

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,31 @@
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE OverloadedLabels      #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 module Spec where
 
 import           Control.Monad               (void)
+import           Control.Monad.Trans.Control
 import           Data.Aeson
 import           Data.ByteString.Char8       (ByteString)
 import qualified Data.ByteString.Lazy        as LBS
 import           Data.Either                 (isRight)
+import           Data.Int                    (Int64)
+import           Data.Maybe                  (fromMaybe)
 import           Data.String.QQ
+import           Data.Text                   (Text)
 import           Data.Time
+import           Melange.DB.Deletions
 import           Melange.DB.Insertions
 import           Melange.DB.Schema
 import           Melange.DB.Selections
+import           Melange.DB.Types
 import           Melange.Model
 import           Squeal.PostgreSQL           hiding (date)
 import           Squeal.PostgreSQL.Migration
@@ -74,6 +85,37 @@ shouldCorrespondTo (Just (Board _ t d its)) bc =
 connectionString :: ByteString
 connectionString = "host=localhost port=5432 dbname=melangetest user=melange password=melange"
 
+type CountQuery = Query Schema '[] '["fromOnly" ::: 'NotNull 'PGint8]
+
+countT :: Has tab schema ('Table table) =>
+  Aliased Alias (alias ::: tab) -> Query schema params '[ "fromOnly" :=> 'NotNull 'PGint8 ]
+countT t =
+  select
+    (countStar `As` #fromOnly :* Nil)
+    (from (table t) & groupBy Nil)
+
+queryNumber :: (MonadBaseControl IO m, MonadPQ Schema m) => CountQuery -> m Int
+queryNumber q =
+  let asInt64 :: (MonadBaseControl IO m, MonadPQ Schema m) => m Int64
+      asInt64 = fromOnly <$> (runQuery q >>= getRow 0)
+  in fromIntegral <$> asInt64
+
+queryNumberOfQuotes :: (MonadBaseControl IO m, MonadPQ Schema m) => m Int
+queryNumberOfQuotes =
+  queryNumber $ countT #quotes
+
+queryNumberOfItems :: (MonadBaseControl IO m, MonadPQ Schema m) => m Int
+queryNumberOfItems =
+  queryNumber $ countT #items
+
+queryNumberOfImages :: (MonadBaseControl IO m, MonadPQ Schema m) => m Int
+queryNumberOfImages =
+  queryNumber $ countT #images
+
+queryNumberOfBoardItems :: (MonadBaseControl IO m, MonadPQ Schema m) => m Int
+queryNumberOfBoardItems =
+  queryNumber $ countT #board_items
+
 exampleJsonSource :: LBS.ByteString
 exampleJsonSource =
   [s|
@@ -95,13 +137,16 @@ dropDB = void . withConnection connectionString $
   manipulate (UnsafeManipulation "SET client_min_messages = error;")
   & pqThen $ migrateDown $ single setup
 
+boardDate :: BoardCreation -> Day
+boardDate BoardCreation{..} = date
+
 main :: IO ()
 main = hspec $ before_ setupDB $ after_ dropDB $ do
   describe "When it comes to boards" $ do
     it "There is no data loss when persisting them and retrieving them" $ do
       pickedBoard <- withConnection connectionString $ do
         void $ newBoard fixtureBoard
-        getBoardByDay ((date :: BoardCreation -> Day) fixtureBoard)
+        getBoardByDay (boardDate fixtureBoard)
       pickedBoard `shouldCorrespondTo` fixtureBoard
 
     it "One can retrieve the latest one" $ do
@@ -114,7 +159,35 @@ main = hspec $ before_ setupDB $ after_ dropDB $ do
       withConnection connectionString (newBoard fixtureBoard >> newBoard fixtureBoard)
         `shouldThrow` (== AlreadyExists)
 
+    it "One can delete a board and it deletes every related items" $ do
+      let boardDay = boardDate fixtureBoard
+      (fetchedBoard, entityCount) <- withConnection connectionString $ do
+          newBoard fixtureBoard >> removeBoardAtDay DeleteNonExistingEntity boardDay
+          b <- getBoardByDay boardDay
+          biN <- queryNumberOfBoardItems
+          bQu <- queryNumberOfQuotes
+          bIm <- queryNumberOfImages
+          bIt <- queryNumberOfItems
+          pure (b, sum [biN, bQu, bIm, bIt])
+      fetchedBoard `shouldBe` Nothing
+      entityCount `shouldBe` 0
+
+    it "One can update a board" $ do
+      updatedBoard <- withConnection connectionString $ do
+        let boardDay = boardDate fixtureBoard
+        void $ newBoard fixtureBoard
+        newBoardFromdDB <- getBoardByDay boardDay
+        let updatableBoard = fromMaybe (error "No board at this date") newBoardFromdDB
+            withUpdate :: Board
+            withUpdate = updatableBoard { boardTitle = Just "Updated title" }
+        _ <- updateBoard boardDay withUpdate
+        getBoardByDay boardDay
+      let getBoardTitle :: Board -> Maybe Text
+          getBoardTitle Board{..} = boardTitle
+      (updatedBoard >>= getBoardTitle) `shouldBe` (Just "Updated title")
+
   describe "Creation items" $
     it "Can be read from JSON" $
       let decoded = eitherDecode exampleJsonSource :: Either String BoardCreation
       in decoded `shouldSatisfy` isRight
+
